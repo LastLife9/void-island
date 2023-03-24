d4387e04faf8efbee3a9bcbc366ccfaac95731af
Heller <hellerkorol@gmail.com>
2023-03-24 18:33:36 +0200

    craft system

---
+++ Assets/_scripts/Heller/Crafting/ItemsManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ItemsManager : MonoBehaviour
+{
+    public static ItemsManager instance;
+    List<ItemScript> itemsScript = new List<ItemScript>();
+    public List<Item> items = new List<Item>();
+    void Awake()
+    {
+        instance = this;
+        BuildItemDatabase();
+    }
+    public ItemScript GetItem(int id)
+    {
+        return itemsScript.Find(item => item.id == id);
+    }
+    public ItemScript GetItem(string name)
+    {
+        return itemsScript.Find(item => item.name == name);
+    }
+    void BuildItemDatabase()
+    {
+        itemsScript = new List<ItemScript>(items.Count);
+        for(int i = 0; i < items.Count; i++)
+        {
+            itemsScript[i] = new ItemScript(items[i]);
+        }
+    }
+    public void TakeItem(string name, int count)
+    {
+        GetItem(name).ChangeItemCount(count);
+    }
+    public void GiveItem(string name, int count)
+    {
+        GetItem(name).ChangeItemCount(-count);
+    }
+}
