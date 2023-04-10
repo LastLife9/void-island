fc2a9144df0ba269cb4bfef00bacf3974f2a2518
LastLife9 <sasha.figol.2001@gmail.com>
2023-04-10 18:43:42 +0300

    fly input controll

--- Assets/_project/_scripts/player/PlayerInputState.cs
+++ Assets/_project/_scripts/player/PlayerInputState.cs
@@ -7,37 +7,50 @@ public class PlayerInputState : MonoBehaviour
 
     private InputState _currentState = InputState.MainMove;
 
+    [SerializeField] private GameObject moveInputPanel;
+    [SerializeField] private GameObject flyInputPanel;
+
     private void Start()
     {
         SendState();
     }
 
-    //private void Update()
-    //{
-    //    if (Input.GetKeyUp(KeyCode.Space))
-    //    {
-    //        if (_currentState == InputState.Main)
-    //        {
-    //            SetState(InputState.Lock);
-    //        }
-    //        else
-    //        {
-    //            SetState(InputState.Main);
-    //        }
-    //        Debug.Log(_currentState);
-    //    }
-    //}
-
     public void SetState(InputState newState)
     {
         _currentState = newState;
 
+        UIUpdate();
         SendState();
     }
 
     public void SendState()
     {
         OnStateChange?.Invoke(_currentState);
+    }
+
+    private void UIUpdate()
+    {
+        switch (_currentState)
+        {
+            case InputState.MainMove:
+                DisableAllPanels();
+                moveInputPanel.SetActive(true);
+                break;
+            case InputState.MainFly:
+                DisableAllPanels();
+                flyInputPanel.SetActive(true);
+                break;
+            case InputState.Lock:
+                DisableAllPanels();
+                break;
+            default:
+                break;
+        }
+    }
+    private void DisableAllPanels()
+    {
+        moveInputPanel.SetActive(false);
+        flyInputPanel.SetActive(false);
     }
 }
 
--- Assets/_project/_scripts/player/PlayerMove.cs
+++ Assets/_project/_scripts/player/PlayerMove.cs
@@ -4,29 +4,33 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    private List<MoveSettings> moveSettings = new List<MoveSettings>();
-    private Dictionary<MoveType, MoveSettings> moveSettingsDict;
-    private MoveSettings _moveSettings;
-
     [Header("Ground")]
-    public float CheckGroundRadius = 0.2f;
+    public float CheckGroundRadius;
     public LayerMask GroundLayers;
 
     [Header("CameraBase")]
     public Transform CharacterTransform;
     public GameObject CinemachineCameraTarget;
 
-    private Rigidbody rb;
-    private Animator anim;
+    private List<MoveSettings> _moveSettings = new List<MoveSettings>();
+    private Dictionary<MoveType, MoveSettings> _moveSettingsDict;
+    private MoveSettings _currentMoveSettings;
+    private MoveType _moveType = MoveType.Walk;
+
+    private Rigidbody _rb;
+    private Animator _anim;
     private GameObject _mainCamera;
 
-    private Vector2 inputVector;
-    private Vector2 look;
-    private Vector2 move;
-    private Vector3 moveVector;
+    private Vector2 _inputVector;
+    private Vector2 _look;
+    private Vector2 _move;
+
+    private Vector3 _smoothMoveVector;
+    private Vector3 _targetDirection;
 
     private bool _isGround = false;
     private bool _jump = false;
+    private bool _fly = false;
 
     private float _rotationVelocityX;
     private float _rotationVelocityY;
@@ -40,10 +44,12 @@ public class PlayerMove : MonoBehaviour
     private int _animIDFreeFall;
     private int _animIDMotionSpeed;
 
+
+    #region MonoBehaviourFunctions
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
-        anim = GetComponentInChildren<Animator>();
+        _rb = GetComponent<Rigidbody>();
+        _anim = GetComponentInChildren<Animator>();
         _mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
@@ -52,10 +58,11 @@ public class PlayerMove : MonoBehaviour
         AssignAnimationIDs();
         AssembleResources();
 
-        _moveSettings = GetSettings(MoveType.Walk);
+        //change settings automatically in upd, based on move type
+        _currentMoveSettings = GetSettings(_moveType);
 
-        _jumpTimeoutDelta = _moveSettings.JumpTimeout;
-        _fallTimeoutDelta = _moveSettings.FallTimeout;
+        _jumpTimeoutDelta = _currentMoveSettings.JumpTimeout;
+        _fallTimeoutDelta = _currentMoveSettings.FallTimeout;
     }
 
     private void Update()
@@ -69,23 +76,49 @@ public class PlayerMove : MonoBehaviour
     {
         CameraRotation();
     }
+    #endregion
 
+
+    #region Initialize
+    private void AssignAnimationIDs()
+    {
+        _animIDForward = Animator.StringToHash("Forward");
+        _animIDRight = Animator.StringToHash("Right");
+        _animIDGrounded = Animator.StringToHash("Grounded");
+        _animIDJump = Animator.StringToHash("Jump");
+        _animIDFreeFall = Animator.StringToHash("FreeFall");
+        _animIDMotionSpeed = Animator.StringToHash("MotionSpeed");
+    }
+
+    private void AssembleResources()
+    {
+        _moveSettings = Resources.LoadAll<MoveSettings>("PlayerMoveSettings").ToList();
+        _moveSettingsDict = _moveSettings.ToDictionary(r => r.Reference, r => r);
+    }
+    #endregion
+
+
+    #region Main
+    private void OnMoveStateChange(MoveType state)
+    {
+        
+    }
     private void Jump()
     {
         if (_isGround)
         {
-            _fallTimeoutDelta = _moveSettings.FallTimeout;
+            _fallTimeoutDelta = _currentMoveSettings.FallTimeout;
 
-            anim.SetBool(_animIDJump, false);
-            anim.SetBool(_animIDFreeFall, false);
+            _anim.SetBool(_animIDJump, false);
+            _anim.SetBool(_animIDFreeFall, false);
 
             if (_jump)
             {
-                float jumpForce = CalculateJumpSpeed(_moveSettings.JumpHeight, Physics.gravity.magnitude);
+                float jumpForce = CalculateJumpSpeed(_currentMoveSettings.JumpHeight, Physics.gravity.magnitude);
 
-                rb.velocity = Vector3.up * jumpForce;
+                _rb.velocity = Vector3.up * jumpForce;
 
-                anim.SetBool(_animIDJump, true);
+                _anim.SetBool(_animIDJump, true);
             }
 
             if (_jumpTimeoutDelta >= 0.0f)
@@ -95,7 +128,7 @@ public class PlayerMove : MonoBehaviour
         }
         else
         {
-            _jumpTimeoutDelta = _moveSettings.JumpTimeout;
+            _jumpTimeoutDelta = _currentMoveSettings.JumpTimeout;
 
             if (_fallTimeoutDelta >= 0.0f)
             {
@@ -103,84 +136,92 @@ public class PlayerMove : MonoBehaviour
             }
             else
             {
-                anim.SetBool(_animIDFreeFall, true);
+                _anim.SetBool(_animIDFreeFall, true);
             }
 
             _jump = false;
         }
     }
-
     private void Move()
     {
-        inputVector = new Vector2(
-            Mathf.Clamp(move.x, _moveSettings.HorizontalLimits.x, _moveSettings.HorizontalLimits.y),
-            Mathf.Clamp(move.y, _moveSettings.VerticalLimits.x, _moveSettings.VerticalLimits.y)
-            );
+        _inputVector = CalculateInput();
+        _smoothMoveVector = CalculateSmoothInput();
+        _targetDirection = CalculateDirection();
 
-        Vector3 _moveVector = new Vector3(inputVector.x, 0, inputVector.y);
-        moveVector = Vector3.Slerp(
-            moveVector,
-            new Vector3(move.x, 0.0f, move.y),
-            _moveSettings.SmoothSpeedMoment * Time.deltaTime);
-
-        float _targetRotation = Mathf.Atan2(_moveVector.x, _moveVector.z)
-            * Mathf.Rad2Deg
-            + _mainCamera.transform.eulerAngles.y;
-        Vector3 targetDirection = 
-            (Quaternion.Euler(0.0f, _targetRotation, 0.0f)
-            * Vector3.forward).normalized
-            * _moveVector.magnitude
-            * moveVector.magnitude;
-
-        rb.velocity = new Vector3(
-            targetDirection.x * _moveSettings.Speed * moveVector.magnitude,
-            rb.velocity.y,
-            targetDirection.z * _moveSettings.Speed * moveVector.magnitude);
-
-        anim.SetFloat(_animIDRight, moveVector.x);
-        anim.SetFloat(_animIDForward, moveVector.z);
-        anim.SetFloat(_animIDMotionSpeed, moveVector.magnitude);
+        switch (_moveType)
+        {
+            case MoveType.Walk:
+                Walk();
+                break;
+            case MoveType.Fly:
+                Fly();
+                break;
+            default:
+                break;
+        }
     }
+    private void Fly()
+    {
 
+    }
+    private void Walk()
+    {
+        _rb.velocity = new Vector3(
+            _targetDirection.x * _currentMoveSettings.Speed * _smoothMoveVector.magnitude,
+            _rb.velocity.y,
+            _targetDirection.z * _currentMoveSettings.Speed * _smoothMoveVector.magnitude);
+
+        _anim.SetFloat(_animIDRight, _smoothMoveVector.x);
+        _anim.SetFloat(_animIDForward, _smoothMoveVector.z);
+        _anim.SetFloat(_animIDMotionSpeed, _smoothMoveVector.magnitude);
+    }
     private void CameraRotation()
     {
         Transform camTargetT = CinemachineCameraTarget.transform;
 
-        float XaxisRotation = look.x;
-        float YaxisRotation = look.y;
+        float XaxisRotation = _look.x;
+        float YaxisRotation = _look.y;
 
         float yRotation = Mathf.SmoothDampAngle(
             camTargetT.eulerAngles.y,
             camTargetT.eulerAngles.y + XaxisRotation * 10f, 
-            ref _rotationVelocityY, _moveSettings.CameraSmoothTimer);
+            ref _rotationVelocityY, _currentMoveSettings.CameraSmoothTimer);
         float xRotation = Mathf.Clamp(Mathf.SmoothDampAngle(
             camTargetT.eulerAngles.x,
             camTargetT.eulerAngles.x - YaxisRotation * 10f, 
-            ref _rotationVelocityX, _moveSettings.CameraSmoothTimer),
-            _moveSettings.BottomClamp, _moveSettings.TopClamp);
+            ref _rotationVelocityX, _currentMoveSettings.CameraSmoothTimer),
+            _currentMoveSettings.BottomClamp, _currentMoveSettings.TopClamp);
 
         camTargetT.rotation = Quaternion.Euler(xRotation, yRotation, 0.0f);
         CharacterTransform.rotation = Quaternion.Slerp(
             CharacterTransform.rotation, 
             Quaternion.Euler(0.0f, yRotation, 0.0f),
-            _moveSettings.CharacterRotationSpeed * Time.deltaTime);
+            _currentMoveSettings.CharacterRotationSpeed * Time.deltaTime);
     }
+    #endregion
 
+
+    #region VirtualInput
     public void VirtualLookInput(Vector2 virtualLookDirection)
     {
-        look = virtualLookDirection;
+        _look = virtualLookDirection;
     }
-
     public void VirtualMoveInput(Vector2 virtualMoveDirection)
     {
-        move = virtualMoveDirection;
+        _move = virtualMoveDirection;
     }
-
     public void VirtualJumpInput()
     {
         _jump = true;
     }
+    public void VirtualFlyInput(bool fly)
+    {
+        _fly = fly;
+    }
+    #endregion
 
+
+    #region Utility
     private void GroundedCheck()
     {
         Vector3 spherePosition = transform.position;
@@ -189,29 +230,40 @@ public class PlayerMove : MonoBehaviour
             GroundLayers,
             QueryTriggerInteraction.Ignore);
 
-        anim.SetBool(_animIDGrounded, _isGround);
+        _anim.SetBool(_animIDGrounded, _isGround);
     }
-
     private float CalculateJumpSpeed(float jumpHeight, float gravity)
     {
         return Mathf.Sqrt(2 * jumpHeight * gravity);
     }
-
-    private void AssignAnimationIDs()
+    private MoveSettings GetSettings(MoveType type)
     {
-        _animIDForward      = Animator.StringToHash("Forward");
-        _animIDRight        = Animator.StringToHash("Right");
-        _animIDGrounded     = Animator.StringToHash("Grounded");
-        _animIDJump         = Animator.StringToHash("Jump");
-        _animIDFreeFall     = Animator.StringToHash("FreeFall");
-        _animIDMotionSpeed  = Animator.StringToHash("MotionSpeed");
+        return _moveSettingsDict[type];
     }
-
-    private void AssembleResources()
+    private Vector2 CalculateInput()
     {
-        moveSettings = Resources.LoadAll<MoveSettings>("PlayerMoveSettings").ToList();
-        moveSettingsDict = moveSettings.ToDictionary(r => r.Reference, r => r);
+        return new Vector2(
+            Mathf.Clamp(_move.x, _currentMoveSettings.HorizontalLimits.x, _currentMoveSettings.HorizontalLimits.y),
+            Mathf.Clamp(_move.y, _currentMoveSettings.VerticalLimits.x, _currentMoveSettings.VerticalLimits.y)
+            );
     }
-
-    public MoveSettings GetSettings(MoveType type) => moveSettingsDict[type];
+    private Vector3 CalculateSmoothInput()
+    {
+        return Vector3.Slerp(
+            _smoothMoveVector,
+            new Vector3(_move.x, 0.0f, _move.y),
+            _currentMoveSettings.SmoothSpeedMoment * Time.deltaTime);
+    }
+    private Vector3 CalculateDirection()
+    {
+        Vector3 _moveVector = new Vector3(_inputVector.x, 0, _inputVector.y);
+        float _targetRotation = Mathf.Atan2(_moveVector.x, _moveVector.z)
+            * Mathf.Rad2Deg
+            + _mainCamera.transform.eulerAngles.y;
+        return (Quaternion.Euler(0.0f, _targetRotation, 0.0f)
+            * Vector3.forward).normalized
+            * _moveVector.magnitude
+            * _smoothMoveVector.magnitude;
+    }
+    #endregion
 }
