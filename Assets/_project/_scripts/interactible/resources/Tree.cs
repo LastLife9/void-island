a900944514c919ab2295b69e39c4ac0bb3ed0c85
LastLife9 <sasha.figol.2001@gmail.com>
2023-05-16 12:52:29 +0300

    leafs animation

--- Assets/_project/_scripts/interactible/resources/Tree.cs
+++ Assets/_project/_scripts/interactible/resources/Tree.cs
@@ -7,6 +7,8 @@ public class Tree : MonoBehaviour
 
     public GameObject baseSkin;
     public GameObject deadSkin;
+    public Transform leafsSkin;
+
     public float ShakeDuration = 0.25f;
     public Vector3 Punch;
 
@@ -29,6 +31,7 @@ public class Tree : MonoBehaviour
         Vector3 playerPosition = _destroyable.Attacker.position;
         Vector3 upVector = Vector3.up;
 
+        //calculate falling tree final rotation
         Vector3 direction = treePosition - playerPosition;
         Quaternion rotation = Quaternion.AngleAxis(90f, Vector3.up);
         Vector3 fallDirection = Vector3.Cross(direction, upVector).normalized;
@@ -36,6 +39,21 @@ public class Tree : MonoBehaviour
         Quaternion finalRotation = Quaternion.FromToRotation(upVector, newFallDirection);
         Vector3 finalRotationVector = finalRotation.eulerAngles;
 
-        transform.DORotate(finalRotationVector, 4f, RotateMode.Fast).SetEase(Ease.InCubic);
+        //calculate leafs rotation
+        Quaternion reverseRotation = Quaternion.AngleAxis(180f, Vector3.up);
+        Vector3 onPlayerDirection = reverseRotation * fallDirection;
+        Quaternion onPlayerFinalRotation = Quaternion.FromToRotation(upVector, onPlayerDirection);
+        Vector3 onPlayerFinalRotationVector = onPlayerFinalRotation.eulerAngles;
+
+        leafsSkin.DORotate(onPlayerFinalRotationVector, 5f, RotateMode.Fast)
+            .SetEase(Ease.InCubic);
+        transform.DORotate(finalRotationVector, 4f, RotateMode.Fast)
+            .SetEase(Ease.InCubic)
+            .OnComplete(OnFallComplete);
+    }
+
+    private void OnFallComplete()
+    {
+        deadSkin.SetActive(false);
     }
 }
